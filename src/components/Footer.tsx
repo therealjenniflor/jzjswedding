e840a8a2902f8154a61b593f1cf73f3ec99a798a
therealjenniflor <jar.zaragoza91@gmail.com>
2026-05-21 13:20:09 -0700

    Mobile: Footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <img className="footer__tumi" src="images/tumi.png" alt="" />
       <div className="footer__content">
         <p>If you have any questions please get in contact with us. We can't wait to celebrate with you.</p>
-        <div className="footer__sig">Jennifer and Jhonatan</div>
+        <div className="footer__sig">Jennifer &amp; Jhonatan</div>
         <div className="footer__meta">¡Salud!</div>
       </div>
       <img className="footer__tumi footer__tumi--right" src="images/tumi.png" alt="" />
